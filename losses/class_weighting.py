fa6e2c58f3fed66a6e73ec93b0ec9575e2e04bf4
Akash Sengupta <akash.sengupta1997@gmail.com>
2019-09-10 23:35:16 +0800

    added median frequency balancing and log inverse probability weighting

--- losses/class_weighting.py
+++ losses/class_weighting.py
@@ -1,24 +1,53 @@
 import os
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
 
-def count_classes(mask_dir, mask_wh, num_classes, mask_format=".png"):
+def pad_to_square_mask(mask):
+    height, width = mask.shape[:2]
+
+    if width < height:
+        border_width = (height - width) // 2
+        padded_mask = cv2.copyMakeBorder(mask, 0, 0, border_width, border_width,
+                                         cv2.BORDER_CONSTANT, value=0)
+    else:
+        border_width = (width - height) // 2
+        padded_mask = cv2.copyMakeBorder(mask, border_width, border_width, 0, 0,
+                                         cv2.BORDER_CONSTANT, value=0)
+    return padded_mask
+
+
+def count_classes(mask_dir, mask_h, mask_w, num_classes, mask_format=".png", padding=True):
     total_counts = np.zeros(num_classes)
 
     for fname in sorted(os.listdir(mask_dir)):
         if fname.endswith(mask_format):
-            mask = cv2.imread(os.path.join(masks_dir, fname), 0)
-            mask = cv2.resize(mask, (mask_wh, mask_wh),
-                              interpolation=cv2.INTER_NEAREST)
-
+            mask = cv2.imread(os.path.join(mask_dir, fname), 0)
+            mask = pad_to_square_mask(mask)
+            mask = cv2.resize(mask, (mask_h, mask_w), interpolation=cv2.INTER_NEAREST)
             counts = np.zeros(32)
             labels, img_counts = np.unique(mask, return_counts=True)
             counts[labels] = img_counts
             total_counts += np.array(counts)
 
+    return total_counts
 
 
+def enet_style_bounded_log_weighting(mask_dir, mask_h, mask_w, num_classes, mask_format=".png",
+                                     c=1.02):
+    class_counts = count_classes(mask_dir, mask_h, mask_w, num_classes,
+                                 mask_format=mask_format)
+    class_probabilities = np.divide(class_counts, np.sum(class_counts))
+    class_weights = np.divide(1.0, np.log(c + class_probabilities))
+
+    return class_weights
 
 
-masks_dir = "/Users/Akash_Sengupta/Documents/4th_year_project_datasets/up-s31/s31_padded_small_glob_rot/masks/train"
+def median_frequency_balancing(mask_dir, mask_h, mask_w, num_classes, mask_format=".png"):
+    class_frequencies = count_classes(mask_dir, mask_h, mask_w, num_classes,
+                                      mask_format=mask_format)
+    median_freq = np.median(class_frequencies)
+    class_weights = np.divide(median_freq, class_frequencies)
+
+    return class_weights
